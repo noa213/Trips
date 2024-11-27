ed9dca72d92af78619ef36c6d80250cd6774e20e
noa <abmagori@gmail.com>
2024-11-27 14:21:52 +0200

    getTrips api

--- src/app/services/trips.tsx
+++ src/app/services/trips.tsx
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { ITrip } from "@/app/types/trip";
 
+export const getTrips = async (): Promise<ITrip[]> => {
+  try {
+    const response = await axios.get("/api/trips");
+    console.log("response get trips: ",response.data);
+    
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching trips:", error);
+    throw new Error("Failed to fetch trips");
+  }
+};
+
 export const addTrip = async (newTrip: ITrip): Promise<ITrip> => {
     try {
       const response = await axios.post("/api/trips", newTrip);
