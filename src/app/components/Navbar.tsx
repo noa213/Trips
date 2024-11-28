25f64a6de82158ee53d947136dca3d906c345484
Ayala tal <ayalakadosh7@gmail.com>
2024-11-28 09:48:02 +0200

    change from noa

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+"use client"
+import Link from 'next/link';
+import React, { useState } from "react";
+// import {
+//   AppBar,
+//   Toolbar,
+//   Typography,
+//   InputBase,
+//   Button,
+//   Tabs,
+//   Tab,
+// } from "@mui/material";
+
+const Navbar = () => {
+  return (
+ <div></div>
+  );
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <></>;
+// export default function Home() {
+//   return <></>;
+// }
+
+
+
+// pages/index.tsx
+import Footer from '@/app/components/Footer'
+// import Navbar from '@/app/components/Navbar'
+
+const Home = () => {
+  return (
+    <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('https://source.unsplash.com/1600x900/?nature,landscape')" }}>
+      {/* <Navbar />
+       */}
+      <div className="flex items-center justify-center min-h-screen bg-opacity-60 bg-black text-white">
+        <h1 className="text-4xl md:text-6xl font-bold text-center">
+          ברוך הבא ל-Triply<br />
+          התחל לתכנן את הטיול הבא שלך!
+        </h1>
+      </div>
+      
+      <Footer />
+    </div>
+  );
 }
+
+export default Home;
