9fae48290bfa5e64090d3a9eb9e92297cbb84df4
noa <abmagori@gmail.com>
2024-12-09 11:21:55 +0200

    test-1-n

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,15 +1,3 @@
-// import CreateTripDialog from "@/app/components/CreateTripDialog";
-// import TripList from "./TripList";
-'use client'
-import { ITask } from "../types/task";
-import CreateTask from "./CreateTask";
-
 export default function HomePage() {
-  return (
-    <div className="p-8">
-
-      {/* <CreateTripDialog /> */}
-    {/* <TripList/> */}
-    </div>
-  );
+  return <div className="p-8"></div>;
 }
