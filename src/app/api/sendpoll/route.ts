7b73f5924c1b263b853c3aa53fcd8fb877207d52
noa <abmagori@gmail.com>
2024-12-12 12:44:46 +0200

    noa4

--- src/app/api/sendpoll/route.ts
+++ src/app/api/sendpoll/route.ts
@@ -1,24 +1,15 @@
-import nodemailer from 'nodemailer';
-import { NextRequest, NextResponse } from 'next/server';
-
-
-
-
+import nodemailer from "nodemailer";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-
-  const { emails, subject, pollResults } =await req.json();
-
+  const { emails, subject, pollResults } = await req.json();
   console.log(emails, subject, pollResults);
-
-  if (!emails || !subject || !pollResults) {
-    return NextResponse.json({status:500,error: 'Missing required fields' })
-  }
-
+  if (!emails || !subject || !pollResults)
+    return NextResponse.json({ status: 500, error: "Missing required fields" });
   try {
     // יצירת transporter לשליחת המייל
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER, // המייל שלך מתוך קובץ הסביבה
         pass: process.env.EMAIL_PASS, // סיסמה או App Password
@@ -28,22 +19,28 @@ export async function POST(req: NextRequest) {
     // הגדרת פרטי המייל
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to: emails.join(','),  // רשימה של מיילים מופרדים בפסיקים
+      to: emails.join(","), // רשימה של מיילים מופרדים בפסיקים
       subject: subject,
       // html: `<h1>${pollResults.message}</h1>`,
-        // שלח את ההודעה בלבד
-        html: `<h1>${'נפתח סקר חדש לגבי הטיול הבא שלך נא היכנס ללינק המצורף על מנת לענות עליו'}</h1>`,
+      // שלח את ההודעה בלבד
+      html: `<h1>${"נפתח סקר חדש לגבי הטיול הבא שלך נא היכנס ללינק המצורף על מנת לענות עליו"}</h1>`,
     };
 
     // שליחת המייל
     const info = await transporter.sendMail(mailOptions);
-  
-    
-   return NextResponse.json({status:500, message: 'Email sent successfully', info });
+    return NextResponse.json({
+      status: 500,
+      message: "Email sent successfully",
+      info,
+    });
   } catch (error) {
     // הדפסת השגיאה בצורה מפורטת
     const e = error as Error;
     console.error("Error sending email:", e.message);
-   return NextResponse.json({status:500, error: 'Failed to send email', details: e.message });
+    return NextResponse.json({
+      status: 500,
+      error: "Failed to send email",
+      details: e.message,
+    });
   }
 }
