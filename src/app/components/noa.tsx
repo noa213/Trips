6d16c2a065e2fb50506725eea97ce4d472261a79
noa <abmagori@gmail.com>
2024-12-15 12:17:24 +0200

    merge master&noa

--- src/app/components/noa.tsx
+++
@@ -1,1 +0,0 @@
-"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpop.education.gov.il%2Ftchumey_daat%2Fgeography_adam_sviva%2Fchativa-elyona%2Fnoseem_nilmadim%2Farim_eyur%2F&psig=AOvVaw0STuMLurjxq7zRNMoZ2b0X&ust=1734090327641000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCND954WUoooDFQAAAAAdAAAAABAE"
